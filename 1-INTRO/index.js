a7029afa431101a322b571898ee0e10550d25917
João Vitor Schaf Nunes <joaovitorschafriva@gmail.com>
2023-04-19 20:55:59 -0300

    Criando arquivos de teste

---
+++ 1-INTRO/index.js
@@ -0,0 +1,7 @@
+var meuNome = "João Nunes";
+var niver = 3;
+function mostrar_nome(nome) {
+    console.log("Meu nome é: " + nome);
+}
+mostrar_nome(meuNome);
+mostrar_nome(niver);
---
+++ 1-INTRO/index.ts
@@ -0,0 +1,9 @@
+const meuNome = "João Nunes"
+const niver = 3
+
+function mostrar_nome(nome: string) {
+  console.log("Meu nome é: " + nome)
+}
+
+mostrar_nome(meuNome);
+mostrar_nome(niver);
