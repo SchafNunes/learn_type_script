6d4e366b3e530061981181ef3f1d8e634ca301ea
João Vitor Schaf Nunes <joaovitorschafriva@gmail.com>
2023-04-19 21:03:44 -0300

    Concluindo o primeiro desafio, função que soma dois valores.

---
+++ 2-DESAFIO/index.js
@@ -0,0 +1,6 @@
+var valorUm = 5;
+var valorDois = 8;
+function soma(valorDois, valorUm) {
+    return valorDois + valorUm;
+}
+console.log(soma(valorDois, valorUm));
---
+++ 2-DESAFIO/index.ts
@@ -0,0 +1,8 @@
+const valorUm = 5
+const valorDois = 8 
+
+function soma(valorDois : number, valorUm : number) {
+  return valorDois + valorUm
+}
+
+console.log(soma(valorDois, valorUm))
