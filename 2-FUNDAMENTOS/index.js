e27dbb1a62246646c7bbbca5e183045d1aaf0c71
João Vitor Schaf Nunes <joaovitorschafriva@gmail.com>
2023-04-19 21:28:03 -0300

    Ajustando estrutura de pastas e brincando com o tipo number

---
+++ 2-FUNDAMENTOS/index.js
@@ -0,0 +1,7 @@
+var x = 10;
+console.log(x);
+console.log(typeof x);
+var y = 114.0000;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision());
---
+++ 2-FUNDAMENTOS/index.ts
@@ -0,0 +1,14 @@
+let x: number = 10
+
+
+console.log(x)
+
+console.log(typeof x)
+
+let y : number = 114.0000
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toPrecision(3))
