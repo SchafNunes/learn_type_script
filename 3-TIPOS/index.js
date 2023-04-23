e94e3a996d51acd24bfd4f814b57c05f2bb67c5e
João Nunes <joaovitorschafriva@gmail.com>
2023-04-23 13:49:54 -0300

    Criando arrays

---
+++ 3-TIPOS/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers[2]);
+//numbers = "Teste"
+const nomes = ["Joao", "Vitor"];
+//nomes.push(65)
---
+++ 3-TIPOS/index.ts
@@ -0,0 +1,11 @@
+let numbers:number[] = [1, 2, 3]
+
+numbers.push(5)
+
+console.log(numbers[2])
+
+//numbers = "Teste"
+
+const nomes: string[] = ["Joao", "Vitor"]
+
+//nomes.push(65)
