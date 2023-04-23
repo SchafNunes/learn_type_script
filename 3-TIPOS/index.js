c7c3eddc33cbf3d3576b83b08b9ba58279212805
João Nunes <joaovitorschafriva@gmail.com>
2023-04-23 13:53:40 -0300

    Testando outra sintaxe de array

--- 3-TIPOS/index.js
+++ 3-TIPOS/index.js
@@ -5,3 +5,9 @@ console.log(numbers[2]);
 //numbers = "Teste"
 const nomes = ["Joao", "Vitor"];
 //nomes.push(65)
+// 2 - outra sintaxe de array
+const nums = [100, 200];
+nums.push(5);
+console.log(nums);
+// nums.push("aaaa")
+console.log(nums[2]);
--- 3-TIPOS/index.ts
+++ 3-TIPOS/index.ts
@@ -9,3 +9,15 @@ console.log(numbers[2])
 const nomes: string[] = ["Joao", "Vitor"]
 
 //nomes.push(65)
+
+// 2 - outra sintaxe de array
+
+const nums:Array<number> = [100, 200]
+
+nums.push(5)
+
+console.log(nums)
+
+// nums.push("aaaa")
+
+console.log(nums[2])
