036cb5bde91406bf02baa69ca5d8a0ad42cb6419
João Nunes <joaovitorschafriva@gmail.com>
2023-04-25 01:41:42 -0300

    trantando possiveis entradas na função

--- 4-NARROWING/index.js
+++ 4-NARROWING/index.js
@@ -1,17 +1,35 @@
+"use strict";
 // 1 - type guard
 function sum(a, b) {
     if (typeof a === 'string' && typeof b === 'string') {
         console.log(parseFloat(a) + parseFloat(b));
-    } else if (typeof a === 'number' && typeof b === 'number') {
-        console.log(a + b)
-    } else {
-        console.log("impossivel realizar a soma")
     }
-
+    else if (typeof a === 'number' && typeof b === 'number') {
+        console.log(a + b);
+    }
+    else {
+        console.log("impossivel realizar a soma");
+    }
 }
-
-
-sum("5", "5")
-sum(5, 5)
-sum("5", 5)
-
+sum("5", "5");
+sum(5, 5);
+sum("5", 5);
+// 2 - checando se valor existe
+function operations(arr, operation) {
+    if (operation) {
+        if (operation === "sum") {
+            const sum = arr.reduce((i, total) => i + total);
+            console.log(sum);
+        }
+        else if (operation === "multiply") {
+            const multiply = arr.reduce((i, total) => i * total);
+            console.log(multiply);
+        }
+    }
+    else {
+        console.log("Por favor, defina uma operação");
+    }
+}
+operations([1, 2, 3]);
+operations([1, 2, 3], "sum");
+operations([1, 2, 3], "multiply");
--- 4-NARROWING/index.ts
+++ 4-NARROWING/index.ts
@@ -1,8 +1,38 @@
-// 1- type guard
+// 1 - type guard
 function sum(a: number | string, b: number | string) {
-    if(typeof a === 'string' && typeof b === 'string') {
-        console.log(parseFloat(a) + parseFloat(b))
+    if (typeof a === 'string' && typeof b === 'string') {
+        console.log(parseFloat(a) + parseFloat(b));
+    } else if (typeof a === 'number' && typeof b === 'number') {
+        console.log(a + b)
+    } else {
+        console.log("impossivel realizar a soma")
+    }
+
+}
+
+
+sum("5", "5")
+sum(5, 5)
+sum("5", 5)
+
+// 2 - checando se valor existe
+function operations(arr: number[], operation?: string | undefined) {
+    if (operation) {
+
+        if (operation === "sum") {
+            const sum = arr.reduce((i, total) => i + total)
+            console.log(sum)
+        }
+        else if (operation === "multiply") {
+            const multiply = arr.reduce((i, total) => i * total)
+            console.log(multiply)
+        }
+    } else {
+        console.log("Por favor, defina uma operação")
     }
 }
 
-sum("5", "5")
+
+operations([1, 2, 3])
+operations([1, 2, 3], "sum")
+operations([1, 2, 3], "multiply")
