db39ddaa754dff13723c77e6499892cb0d0a364b
João Nunes <joaovitorschafriva@gmail.com>
2023-05-02 23:38:58 -0300

    utilizando o operador in para validar se o objeto possui um valor nulo ou <>

--- 4-NARROWING/index.js
+++ 4-NARROWING/index.js
@@ -58,3 +58,24 @@ function userGreeting(user) {
 }
 userGreeting(jhon);
 userGreeting(paul);
+// 4 - Operador in
+class Dog {
+    constructor(name, breed) {
+        this.name = name;
+        if (breed) {
+            this.breed = breed;
+        }
+    }
+}
+const turca = new Dog("Turca");
+const bob = new Dog("Bob", "Pastor Alemão");
+function showDogDetails(dog) {
+    if ('breed' in dog) {
+        console.log(`O cachorro é de raça ${dog.breed}`);
+    }
+    else {
+        console.log("o cachorro é um SRD");
+    }
+}
+showDogDetails(turca);
+showDogDetails(bob);
--- 4-NARROWING/index.ts
+++ 4-NARROWING/index.ts
@@ -69,3 +69,30 @@ function userGreeting(user: object) {
 
 userGreeting(jhon)
 userGreeting(paul)
+
+// 4 - Operador in
+class Dog {
+    name
+    breed
+
+    constructor(name: string, breed?: string) {
+        this.name = name
+        if(breed) {
+            this.breed = breed
+        }
+    }
+}
+
+const turca = new Dog("Turca")
+const bob = new Dog("Bob", "Pastor Alemão")
+
+function showDogDetails(dog: Dog) {
+    if('breed' in dog) {
+        console.log(`O cachorro é de raça ${dog.breed}`)
+    } else {
+        console.log("o cachorro é um SRD")
+    }
+}
+
+showDogDetails(turca)
+showDogDetails(bob)
