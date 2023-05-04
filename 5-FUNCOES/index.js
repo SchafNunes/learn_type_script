8fd949d2f3d29348c5627d409a029e03b08c1f9a
João Nunes <joaovitorschafriva@gmail.com>
2023-05-03 22:43:22 -0300

    utilizando  destructuring

--- 5-FUNCOES/index.js
+++ 5-FUNCOES/index.js
@@ -83,3 +83,9 @@ function sumAll(...n) {
 }
 console.log(sumAll(1, 2, 3, 4, 5));
 console.log(sumAll(145, 2212));
+// 11 - destructuring como parametro
+function showProductDetails({ name, price }) {
+    return `Ó nome do produto é ${name} e ele custa R$${price}`;
+}
+const shirt = { name: "Camisa", price: 49.99 };
+console.log(showProductDetails(shirt));
--- 5-FUNCOES/index.ts
+++ 5-FUNCOES/index.ts
@@ -105,3 +105,12 @@ function sumAll(...n: number[]) {
 }
 console.log(sumAll(1,2,3,4,5))
 console.log(sumAll(145,2212))
+
+// 11 - destructuring como parametro
+function showProductDetails({name, price}: {name:string, price: number}): string {
+    return `Ó nome do produto é ${name} e ele custa R$${price}`
+}
+ const shirt = {name: "Camisa", price: 49.99}
+
+ console.log(showProductDetails(shirt))
+ 
